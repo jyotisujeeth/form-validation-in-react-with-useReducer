f2fa7c7e723bcc220c5cf9f67b17cc784782e95d
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-10-09 12:54:41 +0530

    context  hook

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Card from '../UI/Card/Card';
-import Button from '..UI/Button/Button'
+import Button from '../UI/Button/Button'
 import classes from './Home.module.css';
 
 const Home = (props) => {
